cf98c87a3da4fa76b894e53ca353007c6db84a93
TiagoCGameiro <tiago.condegameiro@gmail.com>
2025-01-09 15:21:42 +0000

    error and payments controllers

---
+++ api/controllers/error.js
@@ -0,0 +1,14 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+export const globalErrorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  sendErrorDev(err, res);
+};
