49fd9adc41e2ae997f98fc0b7cd95331648fd09f
TiagoCGameiro <tiago.condegameiro@gmail.com>
2025-01-14 16:39:46 +0000

    Update error.js

--- api/controllers/error.js
+++ api/controllers/error.js
@@ -1,3 +1,4 @@
+// Dev Environment --------------------------------------------
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -7,6 +8,25 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+// Prod Environment --------------------------------------------
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or unknown error: don't leak details
+    console.error('ERROR 💥', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!',
+    });
+  }
+};
+
+// Global Error Handling --------------------------------------------
 export const globalErrorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
