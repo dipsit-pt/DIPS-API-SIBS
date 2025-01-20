e06e2974ca1d989eabb1169f050b8434f59bc408
TiagoCGameiro <tiago.condegameiro@gmail.com>
2025-01-20 14:59:09 +0000

    route webhook

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 
 // Router --------------------------------------------
 import paymentsRouter from './payments.js';
+import webhookRouter from './webhook.js';
 
 // Routes --------------------------------------------
 // Payments
 router.use('/payments', paymentsRouter);
+
+// Webhook
+router.use('/webhook', webhookRouter);
 
 export default router;
