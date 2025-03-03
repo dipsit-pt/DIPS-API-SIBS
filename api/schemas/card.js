19d95b1ce586697d0520f71a8cd64110fee7dd87
TiagoGameirodipsit <tiago.gameiro@dipsit.pt>
2025-03-03 16:27:59 +0000

    removed the .strict() from headers

--- api/schemas/card.js
+++ api/schemas/card.js
@@ -2,18 +2,16 @@
 import { z } from 'zod';
 
 // Definde headers
-const headers = z
-  .object({
-    'content-type': z.literal('application/json').optional(),
-    'content-length': z.string().optional(),
-    'x-ibm-client-id': z.string(),
-    authorization: z.string().refine((val) => val.startsWith('Digest '), {
-      message: 'Needs an Authorization token',
-    }),
-    accept: z.literal('application/json').optional(),
-    host: z.string().optional(),
-  })
-  .strict();
+const headers = z.object({
+  'content-type': z.literal('application/json').optional(),
+  'content-length': z.string().optional(),
+  'x-ibm-client-id': z.string(),
+  authorization: z.string().refine((val) => val.startsWith('Digest '), {
+    message: 'Needs an Authorization token',
+  }),
+  accept: z.literal('application/json').optional(),
+  host: z.string().optional(),
+});
 
 // Define params
 const params = z
--- api/schemas/webhook.js
+++ api/schemas/webhook.js
@@ -2,16 +2,15 @@
 import { z } from 'zod';
 
 // Definde headers
-const headers = z
-  .object({
-    'x-authentication-tag': z.string(),
-    'x-initialization-vector': z.string(),
-    'content-type': z.literal('text/plain'),
-    'content-length': z.string().optional(),
-    accept: z.string().optional(),
-    host: z.string().optional(),
-  })
-  .strict();
+const headers = z.object({
+  'x-authentication-tag': z.string(),
+  'x-initialization-vector': z.string(),
+  'x-secret-key': z.string(),
+  'content-type': z.literal('text/plain'),
+  'content-length': z.string().optional(),
+  accept: z.string().optional(),
+  host: z.string().optional(),
+});
 
 // Define Body
 const body = z.string();
